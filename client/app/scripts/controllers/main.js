97a8dea674023f5baf2381fb3db317a770304150
MeteTeste <julia.f.koeberle@gmail.com>
2016-10-02 16:46:12 -0400

    finilized starter app

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
---
+++ client/app/scripts/controllers/movies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MoviesCtrl
+ * @description
+ * # MoviesCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MoviesCtrl', function (
+    $scope,
+    Movie
+  ) {
+    $scope.movies = Movie.getList().$object;    //restangular command
+  });
+//Movie is a factory
